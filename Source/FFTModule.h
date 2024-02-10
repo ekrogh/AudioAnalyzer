413fbb226a94f75d2c3301350d1c6a594cd74b01
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-02-11 00:28:03 +0100

    FFT under way.

--- Source/FFTModule.h
+++ Source/FFTModule.h
@@ -139,23 +139,42 @@ public:
 		{
 			if (!nextFFTBlockReady)
 			{
-				fftData = fifo;
-				fftData.resize(2 * fftSize);
-
-				//zeromem(fftData, sizeof(fftData));
-				//memcpy(fftData, fifo, sizeof(fifo));
+				zeromem(fftData, sizeof(fftData));
+				memcpy(fftData, fifo, sizeof(fifo));
 				nextFFTBlockReady = true;
 			}
 
-			fifo.clear();
-			fifo.reserve(0);
 			fifoIndex = 0;
 		}
 
-		fifo.push_back(sample);
-		fifoIndex++;
-		//fifo[fifoIndex++] = sample;
+		fifo[fifoIndex++] = sample;
 	}
+
+	//void pushNextSampleIntoFifo(float sample) noexcept
+	//{
+	//	// if the fifo contains enough data, set a flag to say
+	//	// that the next line should now be rendered..
+	//	if (fifoIndex == fftSize)
+	//	{
+	//		if (!nextFFTBlockReady)
+	//		{
+	//			fftData = fifo;
+	//			fftData.resize(2 * fftSize);
+
+	//			//zeromem(fftData, sizeof(fftData));
+	//			//memcpy(fftData, fifo, sizeof(fifo));
+	//			nextFFTBlockReady = true;
+	//		}
+
+	//		fifo.clear();
+	//		fifo.reserve(0);
+	//		fifoIndex = 0;
+	//	}
+
+	//	fifo.push_back(sample);
+	//	fifoIndex++;
+	//	//fifo[fifoIndex++] = sample;
+	//}
 
 	void drawNextLineOfSpectrogram()
 	{
@@ -166,11 +185,11 @@ public:
 		spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
 
 		// then render our FFT data..
-		forwardFFT->performFrequencyOnlyForwardTransform(fftData.data());
+		forwardFFT->performFrequencyOnlyForwardTransform(fftData);
 
 		// find the range of values produced, so we can scale our rendering to
 		// show up the detail clearly
-		auto maxLevel = FloatVectorOperations::findMinAndMax(fftData.data(), (int)fftSize / 2);
+		auto maxLevel = FloatVectorOperations::findMinAndMax(fftData, (int)fftSize / 2);
 
 		for (auto y = 1; y < imageHeight; ++y)
 		{
@@ -181,6 +200,31 @@ public:
 			spectrogramImage.setPixelAt(rightHandEdge, y, Colour::fromHSV(level, 1.0f, level, 1.0f));
 		}
 	}
+
+	//void drawNextLineOfSpectrogram()
+	//{
+	//	auto rightHandEdge = spectrogramImage.getWidth() - 1;
+	//	auto imageHeight = spectrogramImage.getHeight();
+
+	//	// first, shuffle our image leftwards by 1 pixel..
+	//	spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
+
+	//	// then render our FFT data..
+	//	forwardFFT->performFrequencyOnlyForwardTransform(fftData.data());
+
+	//	// find the range of values produced, so we can scale our rendering to
+	//	// show up the detail clearly
+	//	auto maxLevel = FloatVectorOperations::findMinAndMax(fftData.data(), (int)fftSize / 2);
+
+	//	for (auto y = 1; y < imageHeight; ++y)
+	//	{
+	//		auto skewedProportionY = 1.0f - std::exp(std::log((float)y / (float)imageHeight) * 0.2f);
+	//		auto fftDataIndex = jlimit(0, (int)fftSize / 2, (int)(skewedProportionY * (int)fftSize / 2));
+	//		auto level = jmap(fftData[fftDataIndex], 0.0f, jmax(maxLevel.getEnd(), 1e-5f), 0.0f, 1.0f);
+
+	//		spectrogramImage.setPixelAt(rightHandEdge, y, Colour::fromHSV(level, 1.0f, level, 1.0f));
+	//	}
+	//}
 
 	void handleAudioResource(URL resource)
 	{
@@ -212,13 +256,16 @@ public:
 			return false;
 
 		bufferLengthInSamples = reader->lengthInSamples;
-		fftOrder = std::log2(bufferLengthInSamples) /*/ std::log2(2)*/;
+		fftOrder = std::log2(bufferLengthInSamples);
 		forwardFFT
 			= std::make_unique<dsp::FFT>(fftOrder);
 		fftSize = 1 << fftOrder;
 
+		fifo = new float[fftSize];
+		fftData = new float[2 * fftSize];
 		fifoIndex = 0;
-
+		zeromem(fftData, sizeof(fftData));
+		//floatDataBfrs[0] = fftData;
 		theAudioBuffer =
 			std::make_unique<AudioBuffer<float>>
 			(
@@ -226,26 +273,54 @@ public:
 				,
 				reader->lengthInSamples
 			);
-		auto endTheAudioBuffer =
-			theAudioBuffer->getReadPointer(0) + fftSize * sizeof(float);
 
-		//fftData.assign(theAudioBuffer->getReadPointer(0), endTheAudioBuffer);
-		fftData.resize(2 * fftSize);
-		memcpy(fftData.data(), theAudioBuffer->getReadPointer(0), fftSize * sizeof(float));
+		reader->read
+		(
+			theAudioBuffer.get()
+			,
+			0
+			,
+			(int)reader->lengthInSamples
+			,
+			0
+			,
+			true
+			,
+			true
+		);
 
-		auto tmpFftData = fftData.data();
+		auto rdptrs = theAudioBuffer->getArrayOfReadPointers();
+
+		if (rdptrs[0] != theAudioBuffer->getReadPointer(0))
+		{
+			auto err = 1;
+		}
+		memcpy(fftData, rdptrs[0], sizeof(float) * fftSize);
+
+		juce::dsp::WindowingFunction<float> theHannWindow
+		(
+			fftSize
+			,
+			juce::dsp::WindowingFunction<float>::WindowingMethod::hann
+		);
+
+		theHannWindow.multiplyWithWindowingTable
+		(
+			fftData
+			,
+			fftSize
+		);
 
 		// then render our FFT data..
-		forwardFFT->performFrequencyOnlyForwardTransform(fftData.data(), true);
-
-		tmpFftData = fftData.data();
+		forwardFFT->performFrequencyOnlyForwardTransform(fftData);
+		//forwardFFT->performFrequencyOnlyForwardTransform(fftData, true);
 
 		auto nbrFFTPts = (forwardFFT->getSize() / 2) /*+ 1*/;
-		fftData.resize(nbrFFTPts);
-
+		
 		plotValues.clear();
 		plotValues.reserve(0);
-		plotValues.push_back(fftData);
+		std::vector<float> tmpPVals(fftData, fftData + nbrFFTPts);
+		plotValues.push_back(tmpPVals);
 
 		frequencyValues.clear();
 		frequencyValues.reserve(0);
@@ -267,11 +342,89 @@ public:
 		module_freqPlot->setXLabel("[Hz]");
 		module_freqPlot->setYLabel("[Magnitude]");
 
-		module_freqPlot->updatePlot(plotValues, frequencyValues/*, graph_attributes, plotLegend*/);
-		//module_freqPlot->updatePlot(plotValues, frequencyValues, graph_attributes, plotLegend);
+		module_freqPlot->updatePlot(plotValues, frequencyValues, graph_attributes, plotLegend);
 
 		return true;
 	}
+
+	//bool loadURLIntoFFT(const URL& audioURL)
+	//{
+	//	const auto source = makeInputSource(audioURL);
+
+	//	if (source == nullptr)
+	//		return false;
+
+	//	auto stream = rawToUniquePtr(source->createInputStream());
+
+	//	if (stream == nullptr)
+	//		return false;
+
+	//	reader = rawToUniquePtr(formatManager.createReaderFor(std::move(stream)));
+
+	//	if (reader == nullptr)
+	//		return false;
+
+	//	bufferLengthInSamples = reader->lengthInSamples;
+	//	fftOrder = std::log2(bufferLengthInSamples) /*/ std::log2(2)*/;
+	//	forwardFFT
+	//		= std::make_unique<dsp::FFT>(fftOrder);
+	//	fftSize = 1 << fftOrder;
+
+	//	fifoIndex = 0;
+
+	//	theAudioBuffer =
+	//		std::make_unique<AudioBuffer<float>>
+	//		(
+	//			reader->numChannels
+	//			,
+	//			reader->lengthInSamples
+	//		);
+	//	auto endTheAudioBuffer =
+	//		theAudioBuffer->getReadPointer(0) + fftSize * sizeof(float);
+
+	//	//fftData.assign(theAudioBuffer->getReadPointer(0), endTheAudioBuffer);
+	//	fftData.resize(2 * fftSize);
+	//	memcpy(fftData.data(), theAudioBuffer->getReadPointer(0), fftSize * sizeof(float));
+
+	//	auto tmpFftData = fftData.data();
+
+	//	// then render our FFT data..
+	//	forwardFFT->performFrequencyOnlyForwardTransform(fftData.data(), true);
+
+	//	tmpFftData = fftData.data();
+
+	//	auto nbrFFTPts = (forwardFFT->getSize() / 2) /*+ 1*/;
+	//	fftData.resize(nbrFFTPts);
+
+	//	plotValues.clear();
+	//	plotValues.reserve(0);
+	//	plotValues.push_back(fftData);
+
+	//	frequencyValues.clear();
+	//	frequencyValues.reserve(0);
+	//	auto deltaHz = reader->sampleRate / nbrFFTPts;
+
+	//	std::vector<float> tmpFreqVctr(0);
+	//	float freqVal = 0.0f;
+	//	for (size_t i = 1; i <= nbrFFTPts; i++)
+	//	{
+	//		tmpFreqVctr.push_back(freqVal);
+	//		freqVal += deltaHz;
+	//	}
+	//	frequencyValues.push_back(tmpFreqVctr);
+
+	//	makeGraphAttributes();
+	//	plotLegend.push_back("p " + std::to_string(plotLegend.size() + 1));
+
+	//	module_freqPlot->setTitle("Frequency response [FFT]");
+	//	module_freqPlot->setXLabel("[Hz]");
+	//	module_freqPlot->setYLabel("[Magnitude]");
+
+	//	module_freqPlot->updatePlot(plotValues, frequencyValues/*, graph_attributes, plotLegend*/);
+	//	//module_freqPlot->updatePlot(plotValues, frequencyValues, graph_attributes, plotLegend);
+
+	//	return true;
+	//}
 
 
 	void selectFile()
@@ -314,8 +467,9 @@ private:
 
 	unsigned int fftOrder = 10;
 	unsigned int fftSize = 1 << fftOrder;
-	std::vector<float> fifo;
-	std::vector<float> fftData;
+	float* fifo;
+	float* fftData;
+	float* const* floatDataBfrs[1]{ 0 };
 	int fifoIndex = 0;
 	bool nextFFTBlockReady = false;
 
