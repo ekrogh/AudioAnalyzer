362df0237be88fb8feb1350d4d8fee96f85486ae
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-06-04 00:12:24 +0200

    Enable MP3/LAME audio formats and update SpectrogramComponent
    Enabled MP3 and LAME audio formats across multiple project files. Downgraded `ndkVersionString` and `compileSdkVersion` in `build.gradle`. Added `FFTModule` class for FFT operations and updated `SpectrogramComponent` class with new methods and changes. Removed usage of `yLimTimer` in `SpectrogramComponent`. Added `freqPlotModule` class for plotting frequency data.

---
+++ div/Source/freqPlotModule.h
@@ -0,0 +1,105 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 7.0.9
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2020 - Raw Material Software Limited.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include "cmp_plot.h"
+#include <JuceHeader.h>
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+																	//[Comments]
+	An auto-generated component, created by the Projucer.
+
+	Describe your class and how it works here!
+																	//[/Comments]
+*/
+class freqPlotModule : public juce::Component
+{
+public:
+	//==============================================================================
+	freqPlotModule();
+	~freqPlotModule() override;
+
+	//==============================================================================
+	//[UserMethods]     -- You can add your own custom methods in this section.
+	void xLim(const float min, const float max);
+	void yLim(const float min, const float max);
+
+	void updatePlot
+	(
+		std::vector <std::vector<float>> y_values
+		,
+		std::vector <std::vector<float>> x_values
+	);
+
+	void updatePlot
+	(
+		std::vector <std::vector<float>> y_values
+		,
+		std::vector <std::vector<float>> x_values
+		,
+		cmp::GraphAttributeList graph_attributes
+		,
+		cmp::StringVector legend
+	);
+
+	void updatePlotRealTime(std::vector <std::vector<float>> y_values);
+	void updatePlotRealTime
+	(
+		std::vector <std::vector<float>> y_values
+		,
+		std::vector <std::vector<float>> x_values
+	);
+	void clearPlot();
+	void setXLabel(const std::string& x_label) { m_plot.setXLabel(x_label); };
+	void setYLabel(const std::string& y_label) { m_plot.setYLabel(y_label); };
+	void setTitle(const std::string& title) { m_plot.setTitle(title); };
+	void setXTicks(const std::vector<float>& x_ticks);
+	void setXTickLabels(const std::vector<std::string>& x_labels);
+	void setYTicks(const std::vector<float>& x_ticks);
+	void setYTickLabels(const std::vector<std::string>& x_labels);
+	void plotVerticalLines(const std::vector<float>& x_coordinates,
+		const cmp::GraphAttributeList& graph_attributes = {});
+	//[/UserMethods]
+
+	void paint(juce::Graphics& g) override;
+	void resized() override;
+
+
+
+private:
+	//[UserVariables]   -- You can add your own custom variables in this section.
+	cmp::Plot m_plot;
+	//[/UserVariables]
+
+	//==============================================================================
+
+
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(freqPlotModule)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
